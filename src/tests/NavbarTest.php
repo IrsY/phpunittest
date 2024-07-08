17127938f35482ed8ee5216ce12067dadd8709e6
IrsY <muhdirsyad53@gmail.com>
2024-07-09 01:22:08 +0800

    Update NavbarTest.php

--- src/tests/NavbarTest.php
+++ src/tests/NavbarTest.php
@@ -9,7 +9,9 @@ class NavbarTest extends TestCase {
         session_start(); // Start session
         
         // Set session variables
-        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        if (!isset($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
     }
 
     protected function tearDown(): void {
